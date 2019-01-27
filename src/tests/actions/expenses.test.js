6d13150266ee2eb52a26676e1cf9ad836a0823ef
Henok Seifu <hseifu@jacobs-university.de>
2019-01-27 17:55:29 +0100

    Setup edit expense with firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense, startEditExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -130,4 +130,24 @@ test("should remove expense from firebase", (done) => {
         expect(snapshot.val()).toBeFalsy();
         done();
     });
+})
+
+test("should edit expense inside firebase", (done) => {
+    const store = createMockStore({});
+    const updates = {
+        note: 'new new one'
+    }
+    store.dispatch(startEditExpense(expenses[0].id,updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id: expenses[0].id,
+            updates
+        });
+        return database.ref(`expenses/${expenses[0].id}`).once('value');
+    }).then((snapshot) => {
+        const {id, ...noid} = expenses[0];
+        expect(snapshot.val()).toEqual({...noid, ...updates});
+        done();
+    })
 })
